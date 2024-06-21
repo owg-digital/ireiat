852dc1413afdb86bda6d7daaa67246c65c8587cb
Nick P <nicholas.padon@oliverwyman.com>
2024-06-21 17:15:48 -0500

    Add marine network

--- src/ireiat/data_pipeline/__init__.py
+++ src/ireiat/data_pipeline/__init__.py
@@ -2,7 +2,7 @@ from collections.abc import Sequence
 
 import dagster
 
-from .assets import demand, highway_network, tap, rail_network
+from .assets import demand, highway_network, tap, rail_network, marine_network
 from .io_manager import TabularDataLocalIOManager
 from ..config import CACHE_PATH, INTERMEDIATE_PATH
 
@@ -34,6 +34,14 @@ rail_network_assets = dagster.load_assets_from_package_module(
 #     name="rail_network_job", selection=_filter_for_non_source_assets(rail_network_assets)
 # )
 
+# marine network
+marine_network_assets = dagster.load_assets_from_package_module(
+    marine_network, group_name="marine_network"
+)
+marine_network_assets_job = dagster.define_asset_job(
+    name="marine_network_job", selection=_filter_for_non_source_assets(marine_network_assets)
+)
+
 # tap assets
 tap_assets = dagster.load_assets_from_package_module(tap, group_name="tap")
 tap_assets_job = dagster.define_asset_job(
@@ -41,7 +49,13 @@ tap_assets_job = dagster.define_asset_job(
 )
 
 # all assets
-all_assets = [*demand_assets, *highway_network_assets, *rail_network_assets, *tap_assets]
+all_assets = [
+    *demand_assets,
+    *highway_network_assets,
+    *rail_network_assets,
+    *marine_network_assets,
+    *tap_assets,
+]
 all_assets_job = dagster.define_asset_job(
     name="all", selection=_filter_for_non_source_assets(all_assets)
 )
@@ -49,7 +63,13 @@ all_assets_job = dagster.define_asset_job(
 intermediate_path = str(CACHE_PATH / INTERMEDIATE_PATH)
 defs = dagster.Definitions(
     assets=all_assets,
-    jobs=[demand_assets_job, highway_network_assets_job, tap_assets_job, all_assets_job],
+    jobs=[
+        demand_assets_job,
+        highway_network_assets_job,
+        marine_network_assets_job,
+        tap_assets_job,
+        all_assets_job,
+    ],
     resources={
         "default_io_manager": dagster.FilesystemIOManager(base_dir=str(CACHE_PATH)),
         "default_io_manager_intermediate_path": dagster.FilesystemIOManager(
