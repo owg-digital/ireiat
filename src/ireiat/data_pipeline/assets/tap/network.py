50ef60400f09e9a1237d0f0950168deb91503924
Haseeb Durrani <haseeb.durrani@oliverwyman.com>
2024-09-11 09:32:01 -0400

    - renamed existing tap network dataframe function to tap highway network - placeholder code for rail and marine tap network dataframes

--- src/ireiat/data_pipeline/assets/tap/network.py
+++ src/ireiat/data_pipeline/assets/tap/network.py
@@ -18,12 +18,71 @@ from ireiat.data_pipeline.metadata import publish_metadata
         **INTERMEDIATE_DIRECTORY_ARGS,
     },
 )
-def tap_network_dataframe(
+def tap_highway_network_dataframe(
     context: dagster.AssetExecutionContext, highway_network_dataframe: pd.DataFrame
 ) -> pd.DataFrame:
     """Entire network to represent the TAP, complete with capacity and cost information"""
     # fill out other fields needed for the TAP
     tap_network = highway_network_dataframe
+    tap_network["speed"] = tap_network["speed"].fillna(
+        tap_network["speed"].mean()
+    )  # fill in any null speeds
+    tap_network["fft"] = tap_network["length"] / tap_network["speed"]
+    tap_network["beta"] = HIGHWAY_BETA
+    tap_network["alpha"] = HIGHWAY_ALPHA
+    tap_network["capacity"] = HIGHWAY_CAPACITY_TONS
+    tap_network = tap_network.sort_values(["tail", "head"])
+
+    assert tap_network["speed"].min() > 0
+    publish_metadata(context, tap_network)
+    return tap_network
+
+
+# TODO (HD): TAP RAIL AND MARINE NETWORK DATAFRAMES BELOW
+
+
+@dagster.asset(
+    io_manager_key="custom_io_manager",
+    metadata={
+        "format": "parquet",
+        "write_kwargs": dagster.MetadataValue.json({"index": False}),
+        **INTERMEDIATE_DIRECTORY_ARGS,
+    },
+)
+def tap_rail_network_dataframe(
+    context: dagster.AssetExecutionContext, rail_network_dataframe: pd.DataFrame
+) -> pd.DataFrame:
+    """Entire network to represent the TAP, complete with capacity and cost information"""
+    # fill out other fields needed for the TAP
+    tap_network = rail_network_dataframe
+    tap_network["speed"] = tap_network["speed"].fillna(
+        tap_network["speed"].mean()
+    )  # fill in any null speeds
+    tap_network["fft"] = tap_network["length"] / tap_network["speed"]
+    tap_network["beta"] = HIGHWAY_BETA
+    tap_network["alpha"] = HIGHWAY_ALPHA
+    tap_network["capacity"] = HIGHWAY_CAPACITY_TONS
+    tap_network = tap_network.sort_values(["tail", "head"])
+
+    assert tap_network["speed"].min() > 0
+    publish_metadata(context, tap_network)
+    return tap_network
+
+
+@dagster.asset(
+    io_manager_key="custom_io_manager",
+    metadata={
+        "format": "parquet",
+        "write_kwargs": dagster.MetadataValue.json({"index": False}),
+        **INTERMEDIATE_DIRECTORY_ARGS,
+    },
+)
+def tap_marine_network_dataframe(
+    context: dagster.AssetExecutionContext, marine_network_dataframe: pd.DataFrame
+) -> pd.DataFrame:
+    """Entire network to represent the TAP, complete with capacity and cost information"""
+    # fill out other fields needed for the TAP
+    tap_network = marine_network_dataframe
     tap_network["speed"] = tap_network["speed"].fillna(
         tap_network["speed"].mean()
     )  # fill in any null speeds
