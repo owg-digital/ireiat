5b613a8041497c2f9ef76576381b96ea76ae09e5
Marc Meketon <marc.meketon@oliverwyman.com>
2024-09-27 12:32:23 -0400

    Created simple impedances based on the type of rrs involved

--- src/ireiat/data_pipeline/assets/rail_network/impedance.py
+++ src/ireiat/data_pipeline/assets/rail_network/impedance.py
@@ -1,5 +1,7 @@
 from itertools import chain, product
-from typing import Tuple, Set, Dict, Any
+from typing import Set, Dict, Any
+import dagster
+import pickle
 
 import igraph as ig
 
@@ -36,7 +38,9 @@ def _generate_subgraphs(g: ig.Graph, separation_attribute: str = "owner") -> Dic
     return subgraphs
 
 
-def _generate_impedances(g: ig.Graph, separation_attribute="owner") -> Set[Tuple[str, str]]:
+def _generate_impedances(context: dagster.AssetExecutionContext,
+                         g: ig.Graph,
+                         separation_attribute="owner") -> set[tuple[str, str, str, str]]:
     """
     Given a graph with a `impedance_attribute` on each edge, determine the impedance
     edges that would be needed to join subgraphs that were created from unique values of the
@@ -50,6 +54,7 @@ def _generate_impedances(g: ig.Graph, separation_attribute="owner") -> Set[Tuple
     :return: Set of tuples of impedance graph edges
     """
     impedances = set()
+
     for v in g.vs:
         in_edges, out_edges = v.in_edges(), v.out_edges()
         # cater for single edges
@@ -64,7 +69,8 @@ def _generate_impedances(g: ig.Graph, separation_attribute="owner") -> Set[Tuple
                 # we have single in/out edges with different owners
                 for in_edge_owner, out_edge_owner in product(in_owners, out_owners):
                     if in_edge_owner != out_edge_owner:
-                        impedances.add((f"{in_edge_owner}{v.index}", f"{out_edge_owner}{v.index}"))
+                        impedances.add((f"{in_edge_owner}{v.index}", f"{out_edge_owner}{v.index}",
+                                        f"{in_edge_owner}", f"{out_edge_owner}"))
         else:
             for in_edge in in_edges:
                 in_edge_owners = in_edge[separation_attribute]
@@ -73,37 +79,70 @@ def _generate_impedances(g: ig.Graph, separation_attribute="owner") -> Set[Tuple
                     for in_edge_owner, out_edge_owner in product(in_edge_owners, out_edge_owners):
                         if in_edge_owner != out_edge_owner:
                             impedances.add(
-                                (f"{in_edge_owner}{v.index}", f"{out_edge_owner}{v.index}")
+                                (f"{in_edge_owner}{v.index}", f"{out_edge_owner}{v.index}",
+                                 f"{in_edge_owner}", f"{out_edge_owner}")
                             )
 
     return impedances
 
 
-def generate_impedance_graph(g: ig.Graph, separation_attribute="owner") -> ig.Graph:
+def _determine_impedance(from_rr: str, to_rr: str, impedance_rules: dict) -> float:
+    """Use impedance rules to determine cost of using a railway interchange
+    --Note:  the signature of this function will be expanded to include
+             more information about the nodes in a later release.
+             Keeping it simple for now"""
+
+    # Hard coded.  Unlikely to change in the next 20 years
+    class1_railways = {'BNSF', 'CN', 'CPKC', 'CSXT', 'NS', 'UP'}
+
+    class1_to_class1_default_impedance = impedance_rules['class1_to_class1_default_impedance']
+    class1_to_shortline_default_impedance = impedance_rules['class1_to_shortline_default_impedance']
+
+    impedance = (class1_to_class1_default_impedance if from_rr in class1_railways and to_rr in class1_railways
+                 else class1_to_shortline_default_impedance)
+    return impedance
+
+
+def generate_impedance_graph(context: dagster.AssetExecutionContext,
+                             g: ig.Graph,
+                             rail_interchange_impedances_src: dict,
+                             separation_attribute="owner") -> ig.Graph:
     """Given a graph whose edges all contain `separation_attribute`, return an "exploded"
     graph with impedances edges between vertices that have different values of the
     separation attribute. See the detailed test cases for how this method is intended to function.
 
+    :param context: for logging output
     :param g: iGraph with `separation_attribute` of type `Set`
+    :param rail_interchange_impedances_src: dict version of yaml input file for impedance rules
     :param separation_attribute: string identifying the attribute
     :return: an exploded graph with impedance edges
 
     """
-    impedances = _generate_impedances(g, separation_attribute=separation_attribute)
+    impedances = _generate_impedances(context, g, separation_attribute=separation_attribute)
+
     subgraphs = _generate_subgraphs(g, separation_attribute=separation_attribute)
     disjoint_union = ig.disjoint_union(subgraphs.values())
     disjoint_union_vertex_mapping = {v["original_idx"]: v.index for v in disjoint_union.vs}
+
     impedance_edges = [
-        (disjoint_union_vertex_mapping[from_id], disjoint_union_vertex_mapping[to_id])
-        for from_id, to_id in impedances
+        ((disjoint_union_vertex_mapping[from_id], disjoint_union_vertex_mapping[to_id]),
+         _determine_impedance(from_rr, to_rr, rail_interchange_impedances_src))
+        for from_id, to_id, from_rr, to_rr in impedances
     ]
+
     # construct default edge attributes for impedance edges and add them
     impedance_edge_attrs: Dict[str, Any] = dict()
     impedance_edge_attrs["speed"] = [RAIL_DEFAULT_MPH_SPEED for _ in impedance_edges]
     impedance_edge_attrs["edge_type"] = [EdgeType.IMPEDANCE_LINK.value for _ in impedance_edges]
     impedance_edge_attrs[separation_attribute] = ["imp" for _ in impedance_edges]
-    disjoint_union.add_edges(impedance_edges, impedance_edge_attrs)
+    impedance_edge_attrs["impedance"] = [impedance for _, impedance in impedance_edges]
+    disjoint_union.add_edges([edge for edge, _ in impedance_edges], impedance_edge_attrs)
 
     # eliminate zero degree vertices, preserved when creating subgraphs
     disjoint_union.delete_vertices(disjoint_union.vs.select(_degree=0))
+
+    # Temporary code
+    with open(r'c:\TEMP\impedances.pickle', 'wb') as impedance_file:
+        pickle.dump([disjoint_union, impedances, impedance_edges, rail_interchange_impedances_src], impedance_file)
+
     return disjoint_union
